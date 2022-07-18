5baaac8fcb7e304a4760ec77443263b3de285315
Humm <jhumm@mcw.edu>
2022-07-18 14:23:25 -0500

    feat(jsx):Create DisplaySongs Component and begin song table html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DisplaySongs from './Components/DisplaySongs/DisplaySongs';
 
 
 
 function App() {
-  const [songs, updateAllSongs] = useState([])
-    useEffect(() => {
 
+    const [songs,updateSongs] = useState([]);
 
+    useEffect(()=>{
+       getAllSongs();
+       
     },[]);
 
+    async function getAllSongs(){
+      const response = await axios.get('http://127.0.0.1:8000/music/');
+      updateSongs(response.data);
+      console.log(response.data);
+
+    }
   return (
     <div >
-     
+      <DisplaySongs songs = {songs}/>
     </div>
   );
 }
---
+++ src/Components/DisplaySongs/DisplaySongs.jsx
@@ -0,0 +1,25 @@
+const DisplaySongs  = ({songs}) => {
+    return (  
+<table>
+    <thead>
+        <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+        </tr>
+    </thead>
+
+
+
+
+
+</table>
+
+
+        
+    );
+}
+ 
+export default DisplaySongs;
